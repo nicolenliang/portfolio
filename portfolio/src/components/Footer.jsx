a5892088e38ae0f2d12de1d87d33559ef23f2b4f
Nicole Liang <nicolenliang@gmail.com>
2024-02-01 17:06:03 -0500

    Update Footer/Navbar ui

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
+import Logo from '../../public/logo.png';
+
 const Footer = () => {
     return (
         <>
-            <hr/>
             <div className="footer-container">
+                <p id="back-to-top">back to top</p>
+                <Image src={Logo}
+                    width={160}
+                    height={40}
+                    alt="logo"/>
                 <p>
                     {new Date().getFullYear()} (c) made with love by Nicole Liang
                 </p>
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../public/logo.png";
 
 const Navbar = () => {
     return (
         <div className="nav-container">
             <div className="logo">
                 <Link href="/">
-                    MY PORTFOLIO
+                    <Image 
+                        src={Logo}
+                        width={250}
+                        height={65}
+                        alt="load"/>
                 </Link>
             </div>
-            <a href="" className="cta-btn">Resume</a>
+            <div className="links">
+                <a href="/about" className="nav-link">about</a>
+                <a href="/work" className="nav-link">work</a>
+                <a href="/contact" className="nav-link">contact</a>
+            </div>
         </div>
     )
 }
