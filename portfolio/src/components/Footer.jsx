2deb53bedb070797984770b65d18b142801b8496
Nicole Liang <nicolenliang@gmail.com>
2024-02-01 13:46:08 -0500

    navbar, footer, layout components

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <>
+            <hr/>
+            <div className="footer-container">
+                <p>
+                    {new Date().getFullYear()} (c) made with love by Nicole Liang
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
