473b9919fe1d3525db3aa81aef03b803371ef194
Nicole Liang <nicolenliang@gmail.com>
2024-02-02 00:34:24 -0500

    display components

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,11 +1,23 @@
-import Image from "next/image";
-import { Baskervville } from "next/font/google";
+import Hero from '@/components/Hero';
+import Designs from '@/components/Designs';
+import Contact from '@/components/Contact';
 
-const basker = Baskervville({ subsets: ["latin"], weight: "400" });
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Nicole's Portfolio</title>
+        <meta name="description" content="nicole's portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div>
+        <Hero />
+        <Contact />
+        <p>TEST</p>
+      </div>
     </>
   );
-}
+};
