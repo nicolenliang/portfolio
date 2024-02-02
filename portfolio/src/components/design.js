b703f57afe7a7ea4bd595318b2d15f8bf8abec91
Nicole Liang <nicolenliang@gmail.com>
2024-02-01 22:53:39 -0500

    implement (design) Projects section

---
+++ portfolio/src/components/design.js
@@ -0,0 +1,50 @@
+export const works = [
+    {
+        id: 1,
+        title: 'UI/UX Design',
+        date: '2020 - 23',
+        imgUrl: '/../../public/temp1.jpg',
+    },
+    {
+        id: 2,
+        title: 'Type & Branding',
+        date: '2020 - 23',
+        imgUrl: '/../../public/temp2.jpg',
+    },
+    {
+        id: 3,
+        title: 'Film Photography',
+        date: '2022 - now',
+        imgUrl: '/../../public/temp3.jpg',
+    }
+    {
+        id: 4,
+        title: 'Asian American Student Union',
+        date: '2021 - 23',
+        imgUrl: '/../../public/temp4.jpg',
+    },
+    {
+        id: 5,
+        title: 'Society of Asian Scientists and Engineers',
+        date: '2021-22',
+        imgUrl: '/../../public/temp5.jpg',
+    },
+    {
+        id: 6,
+        title: 'Chinese American Student Association',
+        date: '2020-21',
+        imgUrl: '/../../public/temp6.jpg',
+    },
+    {
+        id: 7,
+        title: 'University of Florida',
+        date: '2022 - 23',
+        imgUrl: '/../../public/temp7.jpg',
+    },
+    {
+        id: 8,
+        title: 'Illustration',
+        date: '2017 - now',
+        imgUrl: '/../../public/temp8.jpg',
+    },
+];
