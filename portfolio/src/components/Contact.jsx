dbb94eb8b382fe6238393137c1eed5f25d3511db
Nicole Liang <nicolenliang@gmail.com>
2024-02-02 00:31:37 -0500

    Create Contact.jsx

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+const Contact = () => {
+    return (
+        <div className="contact-container">
+            <h2>Let's work together :)</h2>
+            <hr/>
+            <div className="contact-column">
+                <a href="/designs">works</a>
+                <a href="/about">about</a>
+                <a href="resume">resume</a>
+            </div>
+            <div className="contact-column">
+                <a href="mailto:nicolenliang@gmail.com">email</a>
+                <a href="https://www.linkedin.com/in/nicolenliang">linkedin</a>
+                <a href="https://instagram.com/nicolenliang">instagram</a>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
