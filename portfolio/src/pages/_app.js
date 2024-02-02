e238caec9dd18c532d704edf95d65082c451d6ad
Nicole Liang <nicolenliang@gmail.com>
2024-02-02 00:40:59 -0500

    change website font

--- portfolio/src/pages/_app.js
+++ portfolio/src/pages/_app.js
@@ -1,10 +1,19 @@
 import "@/styles/globals.css";
 import Layout from '../components/Layout';
+import { Baskervville } from 'next/font/google';
+
+const baskervville = Baskervville({
+  subsets: ['latin'],
+  weight: '400',
+  style: ['normal', 'italic']
+})
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={baskervville.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   );
 }
