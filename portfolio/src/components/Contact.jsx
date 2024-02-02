a90d5e79a38ad4ddc594b5d8a3ce6c41f4ec4fb8
Nicole Liang <nicolenliang@gmail.com>
2024-02-02 00:48:19 -0500

    adding external links to contact

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -6,12 +6,12 @@ const Contact = () => {
             <div className="contact-column">
                 <a href="/designs">works</a>
                 <a href="/about">about</a>
-                <a href="resume">resume</a>
+                <a href="https://docs.google.com/document/d/135y-QE1FMqfJH1lYWlytwadCPVgaNV6iHxpt6Y85Peg/edit?usp=sharing" target="_blank" rel="noreferrer noopener">resume</a>
             </div>
             <div className="contact-column">
-                <a href="mailto:nicolenliang@gmail.com">email</a>
-                <a href="https://www.linkedin.com/in/nicolenliang">linkedin</a>
-                <a href="https://instagram.com/nicolenliang">instagram</a>
+                <a href="mailto:nicolenliang@gmail.com" target="_blank" rel="noreferrer noopener">email</a>
+                <a href="https://www.linkedin.com/in/nicolenliang" target="_blank" rel="noreferrer noopener">linkedin</a>
+                <a href="https://instagram.com/nicolenliang" target="_blank" rel="noreferrer noopener">instagram</a>
             </div>
         </div>
     )
