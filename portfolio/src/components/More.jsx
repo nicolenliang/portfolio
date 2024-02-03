ce910a9a635d2576e9d00182356bc6a229845422
Nicole Liang <nicolenliang@gmail.com>
2024-02-03 01:39:38 -0500

    Create More.jsx

---
+++ portfolio/src/components/More.jsx
@@ -0,0 +1,10 @@
+const More = () => {
+    return (
+        <div className="more-container">
+            <a href="/code">code</a>
+            <a href="/about">about</a>
+        </div>
+    )
+}
+
+export default More;
