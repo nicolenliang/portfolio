3774f0e4a46bac9bbe7158ba0e32662ddcc7137e
Nicole Liang <nicolenliang@gmail.com>
2024-02-03 01:38:00 -0500

    update order of contact component

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,17 +1,19 @@
 const Contact = () => {
     return (
-        <div className="contact-container">
+        <div className="contact-container" id="contact">
             <h2>Let's work together :)</h2>
             <hr/>
-            <div className="contact-column">
-                <a href="/designs">works</a>
-                <a href="/about">about</a>
-                <a href="https://docs.google.com/document/d/135y-QE1FMqfJH1lYWlytwadCPVgaNV6iHxpt6Y85Peg/edit?usp=sharing" target="_blank" rel="noreferrer noopener">resume</a>
-            </div>
-            <div className="contact-column">
-                <a href="mailto:nicolenliang@gmail.com" target="_blank" rel="noreferrer noopener">email</a>
-                <a href="https://www.linkedin.com/in/nicolenliang" target="_blank" rel="noreferrer noopener">linkedin</a>
-                <a href="https://instagram.com/nicolenliang" target="_blank" rel="noreferrer noopener">instagram</a>
+            <div className="contact-items">
+                <ul className="contact-column">
+                    <li><a href="#designs">design</a></li>
+                    <li><a href="/code">code</a></li>
+                    <li><a href="/about">about</a></li>
+                </ul>
+                <ul className="contact-column">
+                    <li><a href="mailto:nicolenliang@gmail.com" target="_blank" rel="noreferrer noopener">email</a></li>
+                    <li><a href="https://www.linkedin.com/in/nicolenliang" target="_blank" rel="noreferrer noopener">linkedin</a></li>
+                    <li><a href="https://docs.google.com/document/d/135y-QE1FMqfJH1lYWlytwadCPVgaNV6iHxpt6Y85Peg/edit?usp=sharing" target="_blank" rel="noreferrer noopener">resume</a></li>
+                </ul>
             </div>
         </div>
     )
