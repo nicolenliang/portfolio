a8ff82acadc8a05f7667a212590a268e11bd954f
Nicole Liang <nicolenliang@gmail.com>
2024-02-03 01:38:24 -0500

    Update Designs.jsx

--- portfolio/src/components/Designs.jsx
+++ portfolio/src/components/Designs.jsx
@@ -2,30 +2,31 @@ import Image from 'next/image';
 import { designData } from './designdata.js';
 
 const Designs = () => {
-    var dataArr = [designData]
-    var half = Math.ceil(dataArr.length / 2);
-    var leftCol = dataArr.slice(0, half);
-    var rightCol = dataArr.slice(half, dataArr.length);
+    var half = Math.ceil(designData.length / 2);
+    var leftCol = designData.slice(0, half);
+    var rightCol = designData.slice(half);
 
     return (
-        <div className="designs-container">
+        <div className="designs-container" id="design">
             <div className="left-column">
-                {leftCol && leftCol.map((project) => (
+                {designData && leftCol.map((project) => (
                     <div className="design-card" key={project.id}>
-                        <Image src={project.imgUrl}
-                        width={300}
-                        height={300}
+                        <Image className="design-image"
+                        src={project.imgUrl}
+                        width={600}
+                        height={600}
                         alt="Project Card"/>
                         <p>{project.title} // {project.date}</p>
                     </div>
                 ))}
             </div>
             <div className="right-column">
-                {rightCol && rightCol.map((project) => (
+                {designData && rightCol.map((project) => (
                     <div className="design-card" key={project.id}>
-                        <Image src={project.imgUrl}
-                        width={300}
-                        height={300}
+                        <Image className="design-image"
+                        src={project.imgUrl}
+                        width={600}
+                        height={600}
                         alt="Project Card"/>
                         <p>{project.title} // {project.date}</p>
                     </div>
