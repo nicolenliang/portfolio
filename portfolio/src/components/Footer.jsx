ef03ef98b471378e85885108325e40cd736c918c
Nicole Liang <nicolenliang@gmail.com>
2024-02-03 01:38:38 -0500

    update footer copyright text

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -6,12 +6,13 @@ const Footer = () => {
         <>
             <div className="footer-container">
                 <p id="back-to-top">back to top</p>
-                <Image src={Logo}
+                <Image className="logo"
+                    src={Logo}
                     width={160}
                     height={40}
                     alt="logo"/>
                 <p>
-                    {new Date().getFullYear()} (c) made with love by Nicole Liang
+                    Made with love &copy; {new Date().getFullYear()} Nicole Liang
                 </p>
             </div>
         </>
