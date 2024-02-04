4d7fb69671810fb186e0528269b9ccd5ae9f8c74
Nicole Liang <nicolenliang@gmail.com>
2024-02-04 01:05:02 -0500

    reformat/style hero section

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,27 +1,20 @@
-import Image from 'next/image';
-import Profile from '../../public/profile.png';
-
 const Hero = () => {
     return (
-        <div className="hero-container" id="about">
-            <Image
-                src={Profile}
-                className="profile"
-                width={488}
-                height={631}
-                alt="Nicole in New York"/>
-            <div className="hero-text">
-                <div>
-                    <h3 className="intro-title">Hi, I'm</h3>
-                    <h1 class="intro-name">Nicole Liang</h1>
-                </div>
-                <hr/>
-                <p>
-                    I'm a recent graduate from the University of Florida, earning my Bachelor's in Computer Science. 
-                    Paired with a long-standing passion for art and design, I'm hoping to explore different paths and 
-                    gain skills and experiences in the intersection of art and technology.
-                </p>
+        <div className="hero-container">
+            <div className="hero-name">
+                <h3>Hi! I'm</h3>
+                <h1>Nicole Liang</h1>
             </div>
+            <hr className="under-name"></hr>
+            <div className="hero-roles">
+                <p><a href="#design">designer</a></p>
+                &nbsp;&nbsp; <p>and</p> &nbsp;&nbsp;
+                <p><a href="/code">software engineer</a></p>
+            </div>
+            <hr />
+            <h4>driven by</h4>
+            <p className="desc">human connection, intentional tech, & empathetic work</p>
+            <p className="quip">(and energy drinks!)</p>
         </div>
     )
 }
