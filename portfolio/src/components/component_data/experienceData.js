15492e4d2557892bc0f531ef17c8d50021ebca27
Nicole Liang <nicolenliang@gmail.com>
2024-02-05 16:31:25 -0500

    Create experienceData.js

---
+++ portfolio/src/components/component_data/experienceData.js
@@ -0,0 +1,32 @@
+export const experienceData = [
+    {
+        title: 'I Can Help With',
+        exp: [
+            'SOFTWARE DEVELOPMENT',
+            'FRONT-END DEVELOPMENT',
+            'GRAPHIC DESIGN',
+            'CREATIVE DIRECTION',
+            'ILLUSTRATION',
+        ],
+    },
+    {
+        title: 'Who Have I Worked With?',
+        exp: [
+            'UNIVERSITY OF FLORIDA',
+            'META PLATFORMS',
+            'TWITCH.TV/DINDINTELEVISION',
+            'SOCIETY OF ASIAN SCIENTISTS & ENGINEERS',
+            'ASIAN AMERICAN STUDENT UNION',
+        ],
+    },
+    {
+        title: 'Software & Technologies',
+        exp: [
+            'ADOBE PHOTOSHOP, ILLUSTRATOR, INDESIGN',
+            'PROCREATE',
+            'FIGMA',
+            'VISUAL STUDIO CODE',
+            'CANVA',
+        ],
+    },
+];
