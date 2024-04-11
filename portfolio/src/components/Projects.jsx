2788925f5561fcff8f7a1b45703ba6d4cd5760bd
Nicole Liang <nicolenliang@gmail.com>
2024-04-11 16:45:28 -0400

    reactive masonry layout for projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,36 +1,38 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+
+const ProjectCard = ( {project} ) => {
+    var projectLink = String(project.shortcut);
+
+    return (
+        <div className="project-card" key={project.id}>
+            <Link href={{ pathname: projectLink, query: project}}>
+                <Image className="project-image"
+                src={project.imgUrl}
+                width={600}
+                height={600}
+                alt="Project Card"/>
+            </Link>
+            <p>{project.title} // {project.date}</p>
+        </div>
+    )
+};
+
 
 const Projects = ( {data} ) => {
-    var half = Math.ceil(data.length / 2);
-    var leftCol = data.slice(0, half);
-    var rightCol = data.slice(half);
+    const columnsCountBreakPoints = {350:1, 750:1, 900:2}
 
     return (
         <div className="project-container">
-            <div className="left-column">
-                {leftCol.map((project) => (
-                    <div className="project-card" key={project.id}>
-                        <Image className="project-image"
-                        src={project.imgUrl}
-                        width={600}
-                        height={600}
-                        alt="Project Card"/>
-                        <p>{project.title} // {project.date}</p>
-                    </div>
-                ))}
-            </div>
-            <div className="right-column">
-                {rightCol.map((project) => (
-                    <div className="project-card" key={project.id}>
-                        <Image className="project-image"
-                        src={project.imgUrl}
-                        width={600}
-                        height={600}
-                        alt="Project Card"/>
-                        <p>{project.title} // {project.date}</p>
-                    </div>
-                ))}
-            </div>
+            <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+                <Masonry gutter={'5rem'}>
+                    {data.map((project) => (
+                        <ProjectCard project={project} />
+                    ))}
+                </Masonry>    
+            </ResponsiveMasonry>
         </div>
     )
 };
