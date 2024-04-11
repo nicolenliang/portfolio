1ad83eab3bbc321e44201f8dbb2fd627e2f47188
Nicole Liang <nicolenliang@gmail.com>
2024-04-11 16:41:42 -0400

    works page working filter

--- portfolio/src/pages/work.jsx
+++ portfolio/src/pages/work.jsx
@@ -1,24 +1,40 @@
-import Link from 'next/link';
 import Contact from '@/components/Contact';
 import Projects from '@/components/Projects';
 import { workData } from '@/components/component_data/workData';
 
+import { useState } from 'react';
+
 const Work = () => {
+    const allData = workData;
+    const [data, setData] = useState(workData);
+
+    const filterData = (event) => {
+        const type = event.target.value;
+        if (type == "all") {
+            setData(allData);
+        }
+        else {
+            const filtered = workData.filter(project => project.type == type);
+            setData(filtered);
+        }
+    }
+
     return (
         <div className="work-container">
             <div className="work-header">
                 <h1>Projects</h1>
                 <hr />
-                <div className="work-meta">
+                <div className="work-desc">
                     <p>personal, professional, and academic projects over time</p>
                     <div className="work-filters">
                         <p>type &mdash; </p>
-                        <Link href="#design">design</Link>
-                        <Link href="#code">code</Link>
+                        <button value="design" onClick={filterData}>design</button>
+                        <button value="code" onClick={filterData}>code</button>
+                        <button value="all" onClick={filterData}>all</button>
                     </div>
                 </div>
             </div>
-            <Projects data={workData} />
+            <Projects data={data} />
             <Contact />
         </div>
     )
