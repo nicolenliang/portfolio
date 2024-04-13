2b9c4004d50ad64775e373a782dd1c30cf13c3a4
Nicole Liang <nicolenliang@gmail.com>
2024-04-13 01:21:07 -0400

    Create filmFiles.js

---
+++ portfolio/src/components/project_files/filmFiles.js
@@ -0,0 +1,122 @@
+export const filmFiles = [
+    {
+        file: '/film/30.jpg',
+        caption: '奶奶 // 2024',
+    },
+    {
+        file: '/film/29.jpg',
+        caption: 'Pondering... // 2023',
+    },
+    {
+        file: '/film/28.jpg',
+        caption: 'Celebration, FL // 2023',
+    },
+    {
+        file: '/film/27.jpg',
+        caption: 'Lulu the Cat // 2023',
+    },
+    {
+        file: '/film/26.jpg',
+        caption: 'Kohaku // 2023',
+    },
+    {
+        file: '/film/25.jpg',
+        caption: 'Kohaku Jazz Bar // 2023',
+    },
+    {
+        file: '/film/24.jpg',
+        caption: 'Kyoto // 2023',
+    },
+    {
+        file: '/film/23.jpg',
+        caption: 'After School // 2023',
+    },
+    {
+        file: '/film/22.jpg',
+        caption: 'Sannen-zaka // 2023',
+    },
+    {
+        file: '/film/21.jpg',
+        caption: 'Monke // 2023',
+    },
+    {
+        file: '/film/20.jpg',
+        caption: 'St. Augustine // 2023',
+    },
+    {
+        file: '/film/19.jpg',
+        caption: 'Cheers in Asheville // 2023',
+    },
+    {
+        file: '/film/18.jpg',
+        caption: 'Amy // 2023',
+    },
+    {
+        file: '/film/17.jpg',
+        caption: 'A Helping Hand // 2023',
+    },
+    {
+        file: '/film/16.jpg',
+        caption: 'MeeT on Main // 2022',
+    },
+    {
+        file: '/film/15.jpg',
+        caption: 'Go Go Curry // 2022',
+    },
+    {
+        file: '/film/14.jpg',
+        caption: 'Central Park // 2022',
+    },
+    {
+        file: '/film/13.jpg',
+        caption: 'Be Gentle When Handling // 2022',
+    },
+    {
+        file: '/film/12.jpg',
+        caption: '小小的 // 2022',
+    },
+    {
+        file: '/film/11.jpg',
+        caption: 'Abrams Falls // 2022',
+    },
+    {
+        file: '/film/10.jpg',
+        caption: 'Reunion, SB // 2022',
+    },
+    {
+        file: '/film/9.jpg',
+        caption: 'A Brief Walk // 2022',
+    },
+    {
+        file: '/film/8.jpg',
+        caption: 'Santa Cruz // 2022',
+    },
+    {
+        file: '/film/7.jpg',
+        caption: 'Big Sur // 2022',
+    },
+    {
+        file: '/film/6.jpg',
+        caption: 'Dog in DR // 2021',
+    },
+    {
+        file: '/film/5.jpg',
+        caption: 'From a Dream, Perhaps // 2021',
+    },
+    {
+        file: '/film/4.jpg',
+        caption: 'A Meal Alone // 2019',
+    },
+    {
+        file: '/film/3.jpg',
+        caption: 'Birds Aren\'t Real // 2019',
+    },
+    {
+        file: '/film/2.jpg',
+        caption: 'Fujiyama // 2019',
+    },
+    {
+        file: '/film/1.jpg',
+        caption: 'Biking // 2019',
+    },
+];
