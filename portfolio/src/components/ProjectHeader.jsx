5fee3ebd90cd10ea378dcbb5a8ab4e2b108d200a
Nicole Liang <nicolenliang@gmail.com>
2024-05-11 00:36:02 -0400

    Replace temp values with project values

--- portfolio/src/components/ProjectHeader.jsx
+++ portfolio/src/components/ProjectHeader.jsx
@@ -3,19 +3,19 @@ import Image from 'next/image';
 const ProjectHeader = ( {project} ) => {
     return (
         <div className="title-container">
-            <div className="title-img">
-                <Image 
-                    src="/temp1.jpg"
-                    width={450}
-                    height={450}
-                    alt="temp"
+            <div>
+                <Image className="title-img"
+                    src={project.imgUrl}
+                    width={400}
+                    height={400}
+                    alt={project.title}
                 />
             </div>
             <div className="title-text">
                 <h1>{project.title}</h1>
                 <hr />
                 <p className="title-year">{project.date}</p>
-                <p>mumbo jumbo {project.desc}</p>
+                <p>{project.desc}</p>
             </div>
         </div>
     )
