2411a8f1a234a0cb6f0ade434d188fdcf58c4fca
Nicole Liang <nicolenliang@gmail.com>
2024-05-11 00:35:31 -0400

    Create ImageCard.jsx

---
+++ portfolio/src/components/ImageCard.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
+
+const ImageCard = ( {filename, caption, onCardClick} ) => {
+    
+    return (
+        <div className="image-card">
+            <button onClick={onCardClick}>
+                <Image className="image-card-image" id={filename}
+                    src={filename}
+                    width={600}
+                    height={600}
+                    alt={caption}
+                />
+                <p>{caption}</p>
+            </button>
+        </div>
+    )
+};
+
+export default ImageCard;
