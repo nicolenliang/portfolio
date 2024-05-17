b253f5409181cdfa76b7ce1dd7e1430f8324f3db
Nicole Liang <nicolenliang@gmail.com>
2024-05-17 15:22:38 -0400

    Update aasu.jsx

--- portfolio/src/pages/aasu.jsx
+++ portfolio/src/pages/aasu.jsx
@@ -1,24 +1,83 @@
 import { useRouter } from 'next/router';
 import ProjectHeader from '@/components/ProjectHeader';
+import More from '@/components/More';
+import ImageCard from '@/components/ImageCard';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { promises as fs } from 'fs';
+import path from 'path';
 
-const Aasu = () => {
+const Aasu = ( {aasuFiles} ) => {
     const router = useRouter();
     var project = router.query;
+
+    const columnsCountBreakPoints = {350:1, 750:3, 900:3}
+
+    const aasuGbm = aasuFiles.filter(file => file.type == "gbm");
+    const aasuMerch = aasuFiles.filter(file => file.type == "merch");
+    const aasuMisc = aasuFiles.filter(file => file.type == "misc");
 
     return (
         <div>
             <ProjectHeader project={project} />
-            <div className="aasu-gbm">
-
+            <div className="proj-imgs-container">
+                <div className="proj-subsec-text">
+                    <h2>General Body Meetings</h2>
+                    <hr />
+                </div>
+                <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+                    <Masonry gutter={'5rem'}>
+                        {aasuGbm.map((file) => (
+                            <ImageCard
+                                filename={file.file}
+                                caption={file.caption}
+                            />
+                        ))}
+                    </Masonry>    
+                </ResponsiveMasonry>
             </div>
-            <div className="aasu-fliers">
-
+            <div className="proj-imgs-container">
+                <div className="proj-subsec-text">
+                    <h2>Merchandise</h2>
+                    <hr />
+                </div>
+                <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+                    <Masonry gutter={'5rem'}>
+                        {aasuMerch.map((file) => (
+                            <ImageCard
+                                filename={file.file}
+                                caption={file.caption}
+                            />
+                        ))}
+                    </Masonry>    
+                </ResponsiveMasonry>
             </div>
-            <div className="aasu-merch">
-                
+            <div className="proj-imgs-container">
+                <div className="proj-subsec-text">
+                    <h2>Miscellaneous Fliers</h2>
+                    <hr />
+                </div>
+                <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+                    <Masonry gutter={'5rem'}>
+                        {aasuMisc.map((file) => (
+                            <ImageCard
+                                filename={file.file}
+                                caption={file.caption}
+                            />
+                        ))}
+                    </Masonry>    
+                </ResponsiveMasonry>
             </div>
+            <More />
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const dir = path.join(process.cwd(), '/src/components/project_files/aasuFiles.json');
+    const file = await fs.readFile(dir, 'utf8');
+    const aasuFiles = JSON.parse(file);
+
+    return { props: { aasuFiles : await Promise.all(aasuFiles) }}
 }
 
 export default Aasu;
