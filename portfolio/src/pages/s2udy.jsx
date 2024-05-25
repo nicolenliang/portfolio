0f8f5e64564d6c17143a55a00c64ba5921560945
Nicole Liang <nicolenliang@gmail.com>
2024-05-25 01:51:51 -0400

    Create s2udy project page

--- portfolio/src/pages/s2udy.jsx
+++ portfolio/src/pages/s2udy.jsx
@@ -1,13 +1,46 @@
-import { useRouter } from 'next/router';
 import ProjectHeader from '@/components/ProjectHeader';
+import ProjectDetails from '@/components/ProjectDetails';
+import More from '@/components/More';
+
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const S2udy = () => {
     const router = useRouter();
-    var project = router.query;
+    const project = router.query;
+    console.log(project)
 
     return (
         <div>
             <ProjectHeader project={project} />
+            <ProjectDetails project={project} />
+            <div className="proj-subsec-container">
+                <div className="proj-subsec-text">
+                    <h2>Overview</h2>
+                    <hr />
+                    <p>{project.overview}</p>
+                </div>
+                <Image className="proj-subsec-img"
+                    src="/s2udy-1.jpg"
+                    width={500}
+                    height={500}
+                    alt="temp"
+                />
+            </div>
+            <div className="proj-subsec-container">
+                <Image className="proj-subsec-img"
+                    src="/s2udy-2.png"
+                    width={500}
+                    height={500}
+                    alt="temp"
+                />
+                <div className="proj-subsec-text">
+                    <h2>Development</h2>
+                    <hr />
+                    <p>{project.development}</p>
+                </div>
+            </div>
+            <More />
         </div>
     )
 }
