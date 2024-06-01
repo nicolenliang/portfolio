13ac89ccfa21c2bfac374bb7d1ad0bfa867c172e
Nicole Liang <nicolenliang@gmail.com>
2024-05-31 20:08:36 -0400

    Hero.jsx formatting

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -9,9 +9,13 @@ const Hero = () => {
             </div>
             <hr className="under-name"></hr>
             <div className="hero-roles">
-                <p><Link href="/work">designer</Link></p>
+                <p><Link href="/work">
+                    designer
+                </Link></p>
                 &nbsp;&nbsp; <p className="and">and</p> &nbsp;&nbsp;
-                <p><Link href="/work">software engineer</Link></p>
+                <p><Link href="/work">
+                    software engineer
+                </Link></p>
             </div>
             <hr />
             <h4>driven by</h4>
