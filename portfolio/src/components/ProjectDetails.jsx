bd54645c3d0938a991e0ab13339206f881a95a8b
Nicole Liang <nicolenliang@gmail.com>
2024-06-14 17:06:22 -0400

    Update ProjectDetails.jsx

--- portfolio/src/components/ProjectDetails.jsx
+++ portfolio/src/components/ProjectDetails.jsx
@@ -1,18 +1,4 @@
-const DetailsList = ( {title, project, cat} ) => {
-    // project is taken in as a router query -- if u refresh the page it does not store project .
-    let info;
-    switch (cat) {
-        case 'role':
-            info = project.role;
-            break;
-        case 'tools':
-            info = project.tools;
-            break;
-        case 'languages':
-            info = project.languages;
-            break;
-    }
-
+const DetailsList = ( {title, info} ) => {
     return (
         <div className="list-container">
             <h3>{title}</h3>
@@ -27,28 +13,24 @@ const DetailsList = ( {title, project, cat} ) => {
 };
 
 const ProjectDetails = ( {project} ) => {
-    // working: render detailslist manually with each category hardcoded in
     return (
         <div className="pd-container">
             <div className="pd-info">
                 <DetailsList
                     title='Role'
-                    project={project}
-                    cat='role'
+                    info={project.role}
                 />
             </div>
             <div className="pd-info">
                 <DetailsList
                     title='Tools'
-                    project={project}
-                    cat='tools'
+                    info={project.tools}
                 />
             </div>
             <div className="pd-info">
                 <DetailsList
                     title='Languages'
-                    project={project}
-                    cat='languages'
+                    info={project.languages}
                 />
             </div>
         </div>
