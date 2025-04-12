808a4601c6b414f4fca02d377529f784a43586de
Nicole Liang <nicolenliang@gmail.com>
2025-04-11 21:47:21 -0400

    Update with Bibimbox project

---
+++ portfolio/src/pages/bibimbox.jsx
@@ -0,0 +1,50 @@
+import ProjectHeader from '@/components/ProjectHeader';
+import ProjectDetails from '@/components/ProjectDetails';
+import More from '@/components/More';
+import ImageCard from '@/components/ImageCard';
+import { projectData } from '@/components/component_data/projectData';
+
+import { useRouter } from 'next/router';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const Bibimbox = ( {bbbFiles} ) => {
+    const router = useRouter();
+    const pathname = router.pathname;
+
+    var project = projectData.filter(project => project.shortcut == pathname);
+    project = project[0];
+
+    const columnsCountBreakPoints = {350:1, 750:2, 900:2}
+    
+    return (
+        <div>
+            <ProjectHeader project={project} />
+            <ProjectDetails project={project} />
+            <div className="proj-imgs-container" id="proj-imgs-container">
+                <ResponsiveMasonry columnsCountBreakPoints={columnsCountBreakPoints}>
+                    <Masonry gutter={'5rem'}>
+                        {bbbFiles.map((file) => (
+                            <ImageCard className="disable-blur"
+                                filename={file.file}
+                                caption={file.caption}
+                            />
+                        ))}
+                    </Masonry>    
+                </ResponsiveMasonry>
+            </div>
+            <More />
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const dir = path.join(process.cwd(), '/src/components/project_files/bbbFiles.json');
+    const file = await fs.readFile(dir, 'utf8');
+    const bbbFiles = JSON.parse(file);
+
+    return { props: { bbbFiles : await Promise.all(bbbFiles) }}
+}
+
+export default Bibimbox;
