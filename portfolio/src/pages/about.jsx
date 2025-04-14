c416cda3fe69d25aa07548feabef2b3f31937538
Nicole Liang <nicolenliang@gmail.com>
2025-04-14 19:37:28 -0400

    Update About page

--- portfolio/src/pages/about.jsx
+++ portfolio/src/pages/about.jsx
@@ -17,30 +17,34 @@ const About = () => {
                     <p className="about-bio">
                     I'm a recent graduate from the University of Florida, earning my Bachelor's in Computer Science. Paired with a long-standing passion for art & design, I hope to pursue experiences that explore the intersection of art and technology.<br /><br />
                     My main objective in both my coding and design work is balancing functionality with appeal. Whether it be making things accessible, clean and concise, or simply more efficient, I have a keen eye for detail and a passion for making things with people first in mind. As a lifelong artist, ex-graphic design major, and current software developer, I enjoy compiling my skills and experiences to work on products that focus on human connection and social impact. With two software internships, multiple coding projects, and four years of design experience under my belt, I’m open to any opportunities that can further inform my disciplines, whether through reinforcing old skills or learning new ones!<br /><br />
-                    When I’m not sat in front of my laptop screen for something or other, you can probably find me struggling on Chess.com, planning my next trip/concert, doing the daily crossword, or — my greatest guilty pleasure — reading the cheesiest novel in existence. I also dabble in creative writing (re: overusing EM dashes), trying every matcha latte on earth (with lavender if possible), and most recently, making handmade ceramics! <br /><br />
+                    When I’m not sat in front of my laptop screen for something or other, you can probably find me fully engrossed in a novel, doing the Sunday crossword (Evan Birholz's are my favorite), or planning my next trip/concert. I also dabble in creative writing (re: overusing parentheses and EM dashes), trying every matcha latte on earth (with lavender if possible), and most recently, making handmade ceramics! <br /><br />
                     Don’t hesitate to say hi back -- I’d love to chat!
                     </p>
                     <div className="socials">
                         <SocialIcon
                             target="_blank"
+                            referrerPolicy="no-referrer"
                             url="https://linkedin.com/in/nicolenliang"
                             bgColor="transparent"
                             fgColor="#5c554b"
                             />
                         <SocialIcon
                             target="_blank"
+                            referrerPolicy="no-referrer"
                             url="https://instagram.com/nicolenliang"
                             bgColor="transparent"
                             fgColor="#5c554b"
                             />
                         <SocialIcon
                             target="_blank"
+                            referrerPolicy="no-referrer"
                             url="https://github.com/nicolenliang"
                             bgColor="transparent"
                             fgColor="#5c554b"
                             />
                         <SocialIcon
                             target="_blank"
+                            referrerPolicy="no-referrer"
                             url="https://open.spotify.com/user/nic4le"
                             bgColor="transparent"
                             fgColor="#5c554b"
