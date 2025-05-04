5087148c118e0aea29c9761991cc09bcd0380376
Nicole Liang <nicolenliang@gmail.com>
2025-05-04 13:35:49 -0400

    Update images for About page

--- portfolio/src/components/AboutImages.jsx
+++ portfolio/src/components/AboutImages.jsx
@@ -4,15 +4,15 @@ const AboutImages = () => {
     return (
         <div className="about-images">
             <Image className="right-img"
-                src="/about-2.png"
-                width={365}
-                height={365}
-                alt="Nicole in Japan, 2023" />
-            <Image className="left-img"
                 src="/about-1.png"
                 width={365}
                 height={365}
                 alt="Nicole in New York, 2021" />
+            <Image className="left-img"
+                src="/about-3.jpg"
+                width={365}
+                height={365}
+                alt="Nicole with a Chantenay Carrot, 2024" />
         </div>
     )
 }
